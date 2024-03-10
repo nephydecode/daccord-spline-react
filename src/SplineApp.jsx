351a212717a9c1db1e6a4871d2bf087a4e9a5db3
nephydecode <nephydecode@gmail.com>
2024-03-11 01:27:51 +0800

    feat: implement SpineApp component

---
+++ src/SplineApp.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+const SplineApp = () => {
+    return ( <Spline scene="https://prod.spline.design/kjaJ-sG-JGkdAFas/scene.splinecode" /> );
+}
+ 
+export default SplineApp;
