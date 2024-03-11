44fb5bf6c3cc4013240dd8a1bd964d67fe6aa653
nephydecode <nephydecode@gmail.com>
2024-03-11 21:00:19 +0800

    ci: update vite.config.js & run build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/daccord-spline-react",
+  build: {
+    outDir: 'build', // Specify the output directory
+  },
   plugins: [react()],
 })
